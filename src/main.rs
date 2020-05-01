2ff79661d9a79038581af9e3328e1f0948970c7c
Tuwuh Sarwoprasojo <tuwuhs@gmail.com>
2020-05-01 07:03:29 -0400

    Try crossterm, it's fun

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 
-use getch::Getch;
+use crossterm::event::{Event, read, KeyCode};
 
 fn main() {
     let _a = 25;
     let b = [1, 2, 3, 4];
-    println!("Hello, world! {:?}", b);
 
-    let key: u8 = Getch::new().getch().unwrap();
-    println!("{}", key);
+    loop {
+        // match read().unwrap() {
+        //     Event::Key(event) => println!("{:?}", event.code),
+        //     _ => (),
+        // }
+
+        if let Event::Key(event) = read().unwrap() {
+            match event.code {
+                KeyCode::Up => println!("YEAH"),
+                KeyCode::Esc => break,
+                _ => (),
+            }
+        }
+    }
+    
+    println!("Hello, world! {:?}", b);
 }
