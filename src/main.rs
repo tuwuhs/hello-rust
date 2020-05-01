997c4a0884c3dff091dc9fe574a413118cf5fed4
Tuwuh Sarwoprasojo <tuwuhs@gmail.com>
2020-05-01 06:21:36 -0400

    Try getch crate, not impressed . . .

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+
+use getch::Getch;
+
 fn main() {
     let _a = 25;
-    let b = [1, 2, 3, 4, 5];
+    let b = [1, 2, 3, 4];
     println!("Hello, world! {:?}", b);
+
+    let key: u8 = Getch::new().getch().unwrap();
+    println!("{}", key);
 }
